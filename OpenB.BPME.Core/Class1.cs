d6a3b55b96201f34affe720d34744d05c1c30717
Wilko <wvanderveen@activium.nl>
2016-07-11 16:33:36 +0200

    Initial commit (for development)

---
+++ OpenB.BPME.Core/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenB.BPME.Core
+{
+    public class ProcessDefinition
+    {
+        public IEnumerable<ProcessStep> Steps { get; set; }
+        public void Execute(ProcessExecutionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+        }
+    }
+
+    public class ProcessStep
+    {
+        public IProcessExpression EntryCriteria { get; set; }
+    }
+}
---
+++ OpenB.BPME.Core/IProcessExpression.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace OpenB.BPME.Core
+{
+    public interface IProcessExpression
+    {
+        IEnumerable<IProcessExpression> SubExpressions { get; }
+        bool Evaluate();
+    }   
+}
